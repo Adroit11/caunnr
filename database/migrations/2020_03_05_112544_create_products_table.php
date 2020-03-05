b07854deecd0cfe3e85aea13390aa0897512c1be
= <kamponistullar@gmail.com>
2020-03-05 13:01:41 +0100

    added products components :wrench: :fire: :clock1:

---
+++ database/migrations/2020_03_05_112544_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('tag_text')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('avatar')->nullable();
+            $table->enum('active', ['yes', 'no'])->default('yes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,13 +1,15 @@
-<div class="w-full flex px-12 bg-white">
-    <div class="w-2/5 flex">
-        <div class="flex-1">
-            Categories
+@foreach($products as $product)
+    <div class="w-full flex px-12 bg-white mb-12">
+        <div class="w-2/5 flex">
+            <div class="flex-1">
+                Categories
+            </div>
+            <div class="flex-1">
+                Image
+            </div>
         </div>
-        <div class="flex-1">
-            Image
+        <div class="w-3/5">
+            product-items
         </div>
     </div>
-    <div class="w-3/5">
-        product-items
-    </div>
-</div>
+@endforeach
