639675a1b2671c4588802928a13385afdeb0d12b
Ogundiji Bolade <kamponistullar@gmail.com>
2020-03-06 16:34:39 +0100

    Ft productscomponent (#10)
    * setting up ci :wrench:
    
    * setting up ci :wrench:
    
    * setting up ci :wrench:
    
    * fix automerge for labels
    
    * fix automerge for labels
    
    * fix labeler error
    
    * tracking errors :wrench:
    
    * tracking errors :wrench:
    
    * tracking errors :wrench:
    
    * tracking errors :wrench:
    
    * tracking errors :wrench:
    
    * moved php insights on PRs :smile: :fire:
    
    * added banner and products :wrench:
    
    * added products components :wrench: :fire: :clock1:
    
    * :wrench: added products components :fire:

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,15 +1,28 @@
-@foreach($products as $product)
-    <div class="w-full flex px-12 bg-white mb-12">
-        <div class="w-2/5 flex">
-            <div class="flex-1">
-                Categories
+@if($products->isNotEmpty())
+    @foreach($products as $product)
+        <div class="w-full flex px-12 bg-white mb-12">
+            <div class="w-2/5 flex">
+                <div class="flex-1 py-12">
+                    <h3 class="text-lg font-semibold">{{$product->name}}</h3>
+                    <div class="mt-6">
+                        <ul class="text-xs font-medium">
+                            @foreach($product->model as $model)
+                                <li>
+                                    <a href="#">{{$model->name}}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="flex-1 py-12">
+                    Image
+                </div>
             </div>
-            <div class="flex-1">
-                Image
+            <div class="w-3/5 py-12">
+                product-items
             </div>
         </div>
-        <div class="w-3/5">
-            product-items
-        </div>
-    </div>
-@endforeach
+    @endforeach
+@else
+    <div>Products are currently not available, please try again</div>
+@endif
