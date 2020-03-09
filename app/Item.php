19a7af7f31f08065ac181889dc2ecd7c8d5d4540
= <kamponistullar@gmail.com>
2020-03-09 17:04:38 +0100

    fix merge issues :wrench: :fire:

--- app/Item.php
+++ app/Item.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-<<<<<<< HEAD
     public function model()
     {
         return $this->belongsTo('App\Model');
     }
-=======
-    //
->>>>>>> 639675a1b2671c4588802928a13385afdeb0d12b
 }
