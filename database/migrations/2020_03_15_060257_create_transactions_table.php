3df102c47f792bd22d097560f540f81b5aab1ea7
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2020-03-15 07:12:32 +0100

    :wrench: added payment gateway

---
+++ database/migrations/2020_03_15_060257_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('reference');
+            $table->double('amount', 8, 2)->nullable();
+            $table->string('status')->nullable();
+            $table->unsignedBigInteger('item_id');
+            $table->index('item_id');
+            $table->unsignedBigInteger('user_id');
+            $table->index('user_id');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
--- public/js/app.js
+++ public/js/app.js
@@ -37150,8 +37150,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\caunnr\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\caunnr\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\newxampp\htdocs\caunnr\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\newxampp\htdocs\caunnr\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
